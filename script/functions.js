c90f7a843317d268fd7c68088744b43ddb039202
tjbass2021 <tjbass2021@gmail.com>
2022-03-06 01:40:19 -0300

    reformulacao do portfolio

--- script/functions.js
+++ script/functions.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 
     let count = false;
+    let countIcons = false;
+    let countComics = false;
 
     $('.eu').mouseover(function(){
         if(count === false){
@@ -15,4 +17,50 @@ $(document).ready(function(){
             count = false;
         };
     })
+
+    $('.icones img').mouseover(function(){
+        if(countIcons === false){
+            $(this).addClass('icones-invert');
+            countIcons = true;
+        }
+    }).mouseout(function(){
+        if(countIcons === true){
+            $(this).removeClass('icones-invert');
+            countIcons = false;
+        }
+    })
+
+    $('.comics').on('click', function(){
+        if(countComics === true){
+            $(this).addClass('bg-aquamarine');
+            $('.ilustras').removeClass('bg-aquamarine');
+            $('.quadrinhos').addClass('d-flex');
+            $('.quadrinhos').removeClass('d-none');
+            $('.ilustracoes').addClass('d-none');
+            $('.ilustracoes').removeClass('d-flex');
+            countComics = false;
+        }
+    });
+
+    $('.ilustras').on('click', function(){
+        if(countComics === false){
+            $(this).addClass('bg-aquamarine');
+            $('.comics').removeClass('bg-aquamarine');
+            $('.ilustracoes').removeClass('d-none');
+            $('.ilustracoes').addClass('d-flex');
+            
+            $('.quadrinhos').addClass('d-none');
+            $('.quadrinhos').removeClass('d-flex');
+            countComics = true;
+
+        }
+    })
+
+    // $('.quadrinhos figure img').on('click', function(){
+    //     if(countComics === false){
+    //         $(this).addClass('position-absolute');
+    //         countComics = true;
+    //     }
+    // })
+
 })
